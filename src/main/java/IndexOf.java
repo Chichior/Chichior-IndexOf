a646e3c432ef2c923b388819d52e44acfb3e1be5
Chichior <katiehakala@icloud.com>
2023-02-26 20:44:04 +0000

    IndexOfComplete

--- src/main/java/IndexOf.java
+++ src/main/java/IndexOf.java
@@ -11,6 +11,34 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
+       
+       for(int i = 0; i < arr.length; i ++){
+            if(arr[i] ==n){
+                return i;
+                
+            }
+            
+       }
+
         return -1;
+       
+        
+
+
+
+       //int x = arr.indexOf(n);
+       
+       
+       
+        //    for(int item : n){
+
+        //     if(item ==n){
+        //         return 1;
+        //     }else{
+
+        
+       
+        //}
+        
     }
 }
